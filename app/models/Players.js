d21c88e8e5b4658844ccd0e3df68b33d066392e2
Camille Ivins <camillemivins@gmail.com>
2023-08-21 15:07:35 -0600

    Not sure if this sent

--- app/models/Players.js
+++ app/models/Players.js
@@ -3,5 +3,18 @@ export class Player {
         this.name = name;
         // The score is defaulted to start at 0
         this.score = 0;
+
+
+
+
+
+        constructor(newName, newScore) {
+            console.log('testing, testing')
+        }
     }
+
+
+
+
+
 }
